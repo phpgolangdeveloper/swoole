8eed60b8b428e0946041e0610769b26b90f81a94
tongwenjie <179355991@qq.com>
2019-07-26 14:43:54 +0800

    fix

--- webSocke/index.php
+++ webSocke/index.php
@@ -1,27 +1,31 @@
 <?php
 
-$server = new swoole_websocket_server("0.0.0.0", 9502);
-//$server->set([]);
-//$server->set(
-//    [
-//        'enable_static_handler' => true,
-//        'document_root' => "/home/work/hdtocs/swoole_mooc/data",
-//    ]
-//);
-//监听websocket连接打开事件
-$server->on('open', 'onOpen');
-function onOpen($server, $request) {
-    print_r($request->fd);
-}
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new \swoole_websocket_server("0.0.0.0", 9502);
 
-// 监听ws消息事件
-$server->on('message', function (swoole_websocket_server $server, $frame) {
-    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-    $server->push($frame->fd, "singwa-push-secesss");
+//监听WebSocket连接打开事件
+$ws->on('open', function ($ws, $request) {
+    var_dump($request->fd, $request->get, $request->server);
+    $ws->push($request->fd, "你好,客户端已经成功和我握手，现在可以通讯啦\n");
 });
 
-$server->on('close', function ($ser, $fd) {
-    echo "client {$fd} closed\n";
+$ws->set(
+    [
+        'worker_num' => 2,
+        'task_worker_num' => 2,
+    ]
+);
+
+
+//监听WebSocket消息事件
+$ws->on('message', function ($ws, $frame) {
+    echo "客户端发来的消息: {$frame->data}\n";
+    $ws->push($frame->fd, "服务端向客户端发的消息: {$frame->data}");
 });
 
-$server->start();
+//监听WebSocket连接关闭事件
+$ws->on('close', function ($ws, $fd) {
+    echo "关闭事件-{$fd} is closed\n";
+});
+
+$ws->start();
