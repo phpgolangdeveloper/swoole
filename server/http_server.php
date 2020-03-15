7781f5f79045a3b7c0711a269f2b4a6081b87a9e
179355991@qq.com <179355991@qq.com>
2020-03-15 23:37:08 +0800

    fix

--- server/http_server.php
+++ server/http_server.php
@@ -2,11 +2,17 @@
 
 // httpserver 是基于server的
 // 监听0.0.0.0 表示监听所有地址
-$http = new swoole_http_server('0.0.0.0', 8811);
+//$http = new swoole_http_server('0.0.0.0', 8811);
+//
+//// $request 请求
+//// $response 响应
+//$http->on('request', function($request, $response) {
+//    $response->end("<h1>HTTPserver</h1>");
+//});
+//$http->start();
 
-// $request 请求
-// $response 响应
-$http->on('request', function($request, $response) {
-    $response->end("<h1>HTTPserver</h1>");
+$http = new Swoole\Http\Server("0.0.0.0", 8811);
+$http->on('request', function ($request, $response) {
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 $http->start();
