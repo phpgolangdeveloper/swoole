66619568412096b4c8286accaaf7caeb1c65fcaf
179355991@qq.com <179355991@qq.com>
2020-03-16 00:02:20 +0800

    fix

--- server/http_server.php
+++ server/http_server.php
@@ -1,18 +1,15 @@
 <?php
-
+// 需要在cli运行，不然会报错的
 // httpserver 是基于server的
 // 监听0.0.0.0 表示监听所有地址
-//$http = new swoole_http_server('0.0.0.0', 8811);
-//
-//// $request 请求
-//// $response 响应
-//$http->on('request', function($request, $response) {
-//    $response->end("<h1>HTTPserver</h1>");
-//});
-//$http->start();
+$http = new swoole_http_server('0.0.0.0', 8811);
 
-$http = new Swoole\Http\Server("0.0.0.0", 8811);
-$http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+// $request 请求
+// $response 响应
+$http->on('request', function($request, $response) {
+//    var_dump($_GET);// 是获取不到的，要通过$request获取
+    var_dump($request->get);
+
+    $response->end("<h1>HTTPserver</h1>");
 });
 $http->start();
