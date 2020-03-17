9b154080e1b8c8f6f4f9bf2d602dc46c6ebfe121
179355991@qq.com <179355991@qq.com>
2020-03-17 23:20:47 +0800

    fix

--- demo/process/curl.php
+++ demo/process/curl.php
@@ -15,10 +15,16 @@ for ($i = 0; $i < 6; $i++) {
     $process = new swoole_process(function (swoole_process $worker) use ($i, $urls) {
         $content = curlData($urls[$i]);
         echo $content . PHP_EOL;
-    }, false);
+    }, true);
     $pid = $process->start();
     $workers[$pid] = $process;
 }
+
+// 获取管道的东西
+foreach ($workers as $process) {
+    echo $process->read();
+}
+
 
 // 模拟请求url 请求耗时为1秒每个
 function curlData($url)
