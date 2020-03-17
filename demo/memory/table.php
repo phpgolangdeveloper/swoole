8f5d47e584ed8934896a20a46e958773cd63c3be
179355991@qq.com <179355991@qq.com>
2020-03-17 23:34:19 +0800

    fix

---
+++ demo/memory/table.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// 创建内存表
+$table = new swoole_table(1024);
+
+
+// 内存表增加一列
+$table->column('id', TYPE_INT);
+$table->column('name', TYPE_STRING);
+$table->column('age', TYPE_INT);
+$table->create();
+
+// 增加一行记录
+$table->set('lalala',
+    ['id' => 1, 'name' => 'twj', 'age' => 18]
+);
+
+$a = $table->get('lalala');
+print_r($a);
