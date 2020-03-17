ee026d6bed89e9487e02e2a52ed6e746b286bebf
179355991@qq.com <179355991@qq.com>
2020-03-17 16:48:28 +0800

    fix

--- demo/io/write.php
+++ demo/io/write.php
@@ -1,11 +1,26 @@
 <?php
 
-
-$filename = './wirte.txt';
-go(function () use ($filename) {
-    sleep(5);
-    echo "end\n";
-    $bool = Swoole\Coroutine\System::writeFile($filename, '哈哈哈哈', FILE_APPEND);
-    var_dump($bool);
+go(function () {
+    a(); // 只新增了一行代码
+    echo "hello go1 \n";
 });
-echo "\nstart";
+
+echo "hello main \n";
+
+go(function () {
+    echo "hello go2 \n";
+});
+
+function a() {
+    sleep(10);
+}
+//
+//
+//$filename = './wirte.txt';
+//go(function () use ($filename) {
+//    sleep(5);
+//    echo "end\n";
+//    $bool = Swoole\Coroutine\System::writeFile($filename, '哈哈哈哈', FILE_APPEND);
+//    var_dump($bool);
+//});
+//echo "\nstart";
