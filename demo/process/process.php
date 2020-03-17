597d573357c8832bb9651b9d19891f6909e38e6f
179355991@qq.com <179355991@qq.com>
2020-03-17 22:36:11 +0800

    fix

--- demo/process/process.php
+++ demo/process/process.php
@@ -1,8 +1,14 @@
 <?php
+// process.php 这个整个是个主进程
 
 $process = new swoole_process(function(swoole_process $pro) {
 
+    // 类似在linux终端里面执行一个命令 类似 php redis.php
+    $pro->exec('/usr/local/php/bin/php',[
+        __DIR__.'../server/http_server.php'
+    ]);// 执行一个外部程序
 
-}, true);
-$pid = $process->start();
+}, false);// true 第二个参数为true,则不会打印在终端了,放管道里面去了
+$pid = $process->start();// 创建一个子进程，返回子进程ID
 echo $pid. PHP_EOL;
+swoole_process::wait();// 回收子进程
