e9822f911136e70e2476e281ccc60294e6153276
179355991@qq.com <179355991@qq.com>
2020-03-17 22:36:49 +0800

    fix

--- demo/process/process.php
+++ demo/process/process.php
@@ -5,7 +5,7 @@ $process = new swoole_process(function(swoole_process $pro) {
 
     // 类似在linux终端里面执行一个命令 类似 php redis.php
     $pro->exec('/usr/local/php/bin/php',[
-        __DIR__.'../server/http_server.php'
+        __DIR__.'/../server/http_server.php'
     ]);// 执行一个外部程序
 
 }, false);// true 第二个参数为true,则不会打印在终端了,放管道里面去了
