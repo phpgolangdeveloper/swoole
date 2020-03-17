82d8a1a3f8eeeb0e4ab6af2bfc62ef8da389a7e1
179355991@qq.com <179355991@qq.com>
2020-03-17 22:05:48 +0800

    fix

--- demo/io/mysql.php
+++ demo/io/mysql.php
@@ -1,42 +1,64 @@
 <?php
+//
+//class AysMysql
+//{
+//
+//    public $dbSource = "";
+//
+//    public function __construct()
+//    {
+////        $this->dbSource = new Swoole\Mysql;
+////        $this->dbConfig = [
+////            'host' => "127.0.0.1",
+////            'post' => 3306,
+////            'user' => 'root',
+////            'password' => 123456,
+////            'database' => 'test',
+////            'charset' => 'utf8',
+////        ];
+//        Co::set(['hook_flags' => SWOOLE_HOOK_TCP]);
+//        $http = new swoole_http_server('0.0.0.0', 8811);
+//        $http->set(['enable_coroutine' => false]);
+//        $http->on('request', function ($request, $response) {
+//            go(function(){
+//                $this->aa();
+//            });
+//
+//            $response->end("hahaha");
+//        });
+//        var_dump(123);
+//        $http->start();
+//    }
+//
+//    public function aa()
+//    {
+//        echo 1;
+//        sleep(10);
+//    }
+//
+//}
+//
+//
+//new AysMysql();
 
-class AysMysql
-{
+$server = new Swoole\Http\Server("0.0.0.0", 8811);
 
-    public $dbSource = "";
+$server->set([
+//关闭内置协程
+    'enable_coroutine' => false,
+]);
 
-    public function __construct()
-    {
-//        $this->dbSource = new Swoole\Mysql;
-//        $this->dbConfig = [
-//            'host' => "127.0.0.1",
-//            'post' => 3306,
-//            'user' => 'root',
-//            'password' => 123456,
-//            'database' => 'test',
-//            'charset' => 'utf8',
-//        ];
-        Co::set(['hook_flags' => SWOOLE_HOOK_TCP]);
-        $http = new swoole_http_server('0.0.0.0', 8811);
-        $http->set(['enable_coroutine' => false]);
-        $http->on('request', function ($request, $response) {
-            go(function(){
-                $this->aa();
-            });
-
-            $response->end("hahaha");
+$server->on("request", function ($request, $response) {
+    if ($request->server['request_uri'] == '/coro') {
+        go(function () use ($response) {
+            co::sleep(0.2);
+            $response->header("Content-Type", "text/plain");
+            $response->end("Hello World\n");
         });
-        var_dump(123);
-        $http->start();
+    } else {
+        $response->header("Content-Type", "text/plain");
+        $response->end("Hello World\n");
     }
+});
 
-    public function aa()
-    {
-        echo 1;
-        sleep(10);
-    }
-
-}
-
-
-new AysMysql();
+$server->start();
