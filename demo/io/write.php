558a7992a3048d2d97615a23ece308574f8c0f0c
179355991@qq.com <179355991@qq.com>
2020-03-17 16:21:47 +0800

    fix

--- demo/io/write.php
+++ demo/io/write.php
@@ -3,6 +3,7 @@
 
 $filename = './wirte.txt';
 go(function () use ($filename) {
+    sleep(5);
     echo "end\n";
     $bool = Swoole\Coroutine\System::writeFile($filename, '哈哈哈哈', FILE_APPEND);
     var_dump($bool);
