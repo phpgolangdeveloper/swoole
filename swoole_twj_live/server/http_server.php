210810dad15463b80c09c144dccc65296f8316e2
179355991@qq.com <179355991@qq.com>
2020-03-19 23:20:57 +0800

    fi

--- swoole_twj_live/server/http_server.php
+++ swoole_twj_live/server/http_server.php
@@ -8,20 +8,49 @@ $http->set(
     [
         'enable_static_handler' => true,//开启静态文件请求处理功能，需配合 document_root 使用 默认 false
         'document_root' => '/home/wwwroot/default/twj/swoole/swoole_twj_live/public/static',//设置静态处理器的路径。类型为数组，默认不启用
-        'worker_num' => 5,
+        'worker_num' => 5, // 五个worker进程，这里有多少个就会有多少个WorkerStart回调
     ]
 );
 
-$http->on('WorkerStart', function(swoole_server $server, $worker_id ) {
-    define('APP_PATH',__DIR__.'/../application');
-    require __DIR__.'/../thinkphp/base.php';
+$http->on('WorkerStart', function (swoole_server $server, $worker_id) {
+    // 在worker进程中把相关代码加载进来
+    define('APP_PATH', __DIR__ . '/../application');
+    require __DIR__ . '/../thinkphp/base.php';
 });
 
 // 上面$htt->set()，如果它有静态资源，就不会再走后面的逻辑了
 $http->on('request', function ($request, $response) {
 
+    if (isset($request->server)) {
+        foreach ($response->server as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if (isset($request->header)) {
+        foreach ($response->header as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if (isset($request->get)) {
+        foreach ($response->get as $k => $v) {
+            $_GET[strtoupper($k)] = $v;
+        }
+    }
+
+    if (isset($request->post)) {
+        foreach ($response->post as $k => $v) {
+            $_POST[strtoupper($k)] = $v;
+        }
+    }
+
+    \think\Container::get('app', [APP_PATH])
+        ->run()->send();
+
     $response->cookie('singwa', '值', time() + 1800);
     $response->end("<h1>HTTPserver</h1>");
+
+
 
 });
 $http->start();
