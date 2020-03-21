ed2a5bbaadd84ffa45c95e5fa8b0975ee612d29e
179355991@qq.com <179355991@qq.com>
2020-03-21 22:23:09 +0800

    fix

--- swoole_twj_live/server/http_server.php
+++ swoole_twj_live/server/http_server.php
@@ -44,7 +44,7 @@ $http->on('request', function ($request, $response) use ($http) {
             $_POST[strtoupper($k)] = $v;
         }
     }
-    ob_start();
+//    ob_start();
     try {
         \think\Container::get('app', [APP_PATH])
             ->run()
@@ -52,9 +52,9 @@ $http->on('request', function ($request, $response) use ($http) {
     } catch (\Exception $e) {
 
     }
-    echo 'action-'.$request()->action().PHP_EOL;
-    $res = ob_get_contents();
-    ob_end_clean();
+    $res = 'action-' . $request()->action() . PHP_EOL;
+//    $res = ob_get_contents();
+//    ob_end_clean();
     $response->end($res);
     $http->close();
 
